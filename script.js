1577b23d7a54e98f7f6800789e80bb21014ca30a
RKBDI <radekxmen.bledowski@gmail.com>
2025-07-30 00:14:04 +0200

    fixed sizes and added more icons

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let activeIndex = 1;
 
 function updateActive(index) {
   icons.forEach((icon, i) => icon.classList.toggle('active', i === index));
-  const offset = (index - 1) * 480;
+  const offset = (index - 1) * 500;
   carousel.style.transform = `translateX(-${offset}px)`;
   title.textContent = icons[index].dataset.title;
 }
